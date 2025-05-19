aac4c226f79163f3520f4f36809e95d4230aefc4
Mceow137 <ivanzujkov569@gmail.com>
2025-05-19 16:43:45 +0300

    Add files via upload

---
+++ medlaaab/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BarcodeServiceTests
+    {
+        [TestMethod]
+        public void GenerateBarcode_ReturnsValidFormat()
+        {
+            // Arrange
+            var barcodeService = new BarcodeService();
+            var orderId = 123;
+            var expectedPattern = @"^\d{3} \d{8} \d{6}$"; // 123 01112023 123456
+
+            // Act
+            var result = barcodeService.GenerateBarcode(orderId);
+
+            // Assert
+            Assert.IsTrue(Regex.IsMatch(result, expectedPattern));
+        }
+    }
+
+    internal class BarcodeService
+    {
+        public BarcodeService()
+        {
+        }
+
+        internal string GenerateBarcode(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
